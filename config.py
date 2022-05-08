8f833b4bd21145bb21e95a75efbc3f6784887424
Abdi Ali <alikune33@gmail.com>
2022-05-08 14:46:04 +0300

    create config.py content

---
+++ config.py
@@ -0,0 +1,39 @@
+from email.policy import default
+import os
+from decouple import config
+
+class Config:
+    """
+    General configuration parent class
+    """
+    DB_USER = config('DB_USER', default="")
+    DB_PASSWORD = config('DB_PASSWORD', default="")
+    DB = 'pitches'
+
+    UPLOADED_PHOTOS_DEST ='app/static/uploads'
+    
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@localhost/{DB}'
+
+    SECRET_KEY = config('SECRET_KEY', default="")
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = config("MAIL_USERNAME", default="")
+    MAIL_PASSWORD = config("MAIL_PASSWORD", default="")
+
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = config("DATABASE_URL", default="").replace("postgres://", "postgresql://", 1)
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+    'development': DevelopmentConfig,
+    'production': ProdConfig
+}
